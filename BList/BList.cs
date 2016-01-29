0e6b741bfdc4f7ace0a1dff543f00b6493c54295
Meirion Hughes <mhughes@geologging.com>
2016-01-29 16:57:50 +0000

    Add main files; Blist failing tests.

---
+++ BList/BList.cs
@@ -0,0 +1,206 @@
+﻿using System.Linq;
+
+namespace System.Collections.Generic
+{
+    public class BList<T> : IList<T>, IReadOnlyList<T>
+    {
+        private const int InitialCapacity = 4;
+        private const int InitialOffset = 2;
+
+        private int _size;
+        private int _offset;
+        private int _capacity;
+
+        private T[] _items;
+
+        public BList()
+        {
+            _size = 0;
+            _offset = InitialOffset;
+            _capacity = InitialCapacity;
+            _items = new T[InitialCapacity];
+        }
+
+
+
+        private void Insert(int insertIndex, T[] insertItems)
+        {
+            if (insertIndex < 0)
+                throw new ArgumentOutOfRangeException(nameof(insertIndex));
+
+            var insertCount = insertItems.Length;
+
+            var padRight = Math.Max(0, (insertIndex == 0)
+                ? (insertCount - (_size + 1))
+                : (insertIndex + insertCount) - (_size));
+
+            var padLeft = insertIndex == 0
+                ? insertCount
+                : 0;
+
+            var requiresResize = _offset - padLeft < 0 ||
+                                 _offset + _size + padRight > _capacity;
+
+            if (requiresResize)
+            {
+                var newSize = _size + insertCount;
+                var newCapacity = Math.Max(newSize, _capacity*2);
+                var newOffset = (newCapacity/2) - (newSize/2) - padLeft;
+                var newItems = new T[newCapacity];
+
+
+                for (int i = 0; i < insertIndex; i++)
+                    newItems[newOffset + i] = _items[_offset + i];
+        
+                for (int i = insertIndex; i < _size; i++)
+                    newItems[newOffset + i + insertCount] = _items[_offset + i];
+
+                for (int i = 0; i < insertCount; i++)
+                    newItems[newOffset + insertIndex + i] = insertItems[i];
+        
+                _items = newItems;
+                _offset = newOffset;
+                _size = newSize;
+                _capacity = newCapacity;
+            }
+            else
+            {
+
+                if (insertIndex != 0)
+                {
+                    //for (int i = 0; i < insertIndex; i++)
+                    //    _items[_offset + i] = _items[_offset + i];
+
+                    for (int i = _size - 1; i >= insertIndex; i--)
+                        _items[_offset + i + insertCount] = _items[_offset + i];
+
+                    for (int i = 0; i < insertCount; i++)
+                        _items[_offset + insertIndex + i] = insertItems[i];
+                }
+                else
+                {
+                    _offset = _offset - insertCount;
+
+                    for (int i = 0; i < insertCount; i++)
+                        _items[_offset + insertIndex + i] = insertItems[i];
+                }
+
+                _size = _size + insertCount;
+            }
+
+        }
+
+        public int Count => _size;
+
+        public bool IsReadOnly => false;
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < _size ; i++)
+            {
+                if (_items[i+_offset].Equals(item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public bool Remove(T item)
+        {
+            var index = IndexOf(item);
+
+            if (index == -1)
+                return false;
+
+            RemoveAt(index);
+
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index == 0)
+            {
+                _items[_offset] = default(T);
+                _offset++;
+                _size -= 1;
+            }
+            else if (index == _size - 1)
+            {
+                _items[_offset + _size - 1] = default(T);
+                _size -= 1;
+            }
+            else
+            {
+                for (int i = _offset + index; i < _size - 1; i++)
+                    _items[i] = _items[i + 1];
+
+                _size -= 1;
+            }
+        }
+
+        public T this[int index]
+        {
+            get { return _items[_offset + index]; }
+            set { Insert(index, new[] { value }); }
+        }
+
+        public void Insert(int insertIndex, T item)
+        {
+            Insert(insertIndex, new[] { item });
+        }
+        
+        public void Add(T item)
+        {
+            Insert(Count, new[] { item });
+        }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            Insert(_size, collection.ToArray());
+        }
+
+        public void Clear()
+        {
+            _offset = _capacity / 2;
+            _size = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            if ((Object)item == null)
+            {
+                for (int i = 0; i < _size; i++)
+                    if ((Object)_items[i] == null)
+                        return true;
+                return false;
+            }
+            else
+            {
+                EqualityComparer<T> c = EqualityComparer<T>.Default;
+                for (int i = 0; i < _size; i++)
+                {
+                    if (c.Equals(_items[i], item)) return true;
+                }
+                return false;
+            }
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Array.Copy(_items, 0, array, arrayIndex, _size);
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = _offset; i < _offset + _size; i++)
+                yield return _items[i];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    };
+}
---
+++ BList/BListTests.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Testing
+{
+    [TestFixture]
+    public class BListTests
+    {
+        private Random _random = new Random();
+
+        [Test]
+        public void given_empty_blist_when_one_element_is_added_then_blist_count_should_equal_one()
+        {
+            var count = 1;
+            var expected = new int[count].Select(_ => _random.Next()).ToArray();
+
+            var blist = new BList<int>();
+
+            foreach (var item in expected)
+                blist.Add(item);
+
+            Assert.That(blist.Count, Is.EqualTo(count));
+        }
+
+        [Test]
+        public void given_empty_blist_when_one_element_is_added_then_blist_indexer_should_return_element()
+        {
+            var count = 1;
+            var expected = new int[count].Select(_ => _random.Next()).ToArray();
+
+            var blist = new BList<int>();
+
+            foreach (var item in expected)
+                blist.Add(item);
+
+            Assert.That(expected[0], Is.EqualTo(blist[0]));
+        }
+
+        [Test]
+        public void given_empty_blist_when_one_element_is_added_then_blist_iterator_should_return_element()
+        {
+            var count = 1;
+            var expected = new int[count].Select(_ => _random.Next()).ToArray();
+
+            var blist = new BList<int>();
+
+            foreach (var item in expected)
+                blist.Add(item);
+
+            CollectionAssert.AreEqual(expected, blist);
+        }
+
+        [Test]
+        public void given_empty_blist_when_one_element_is_inserted_at_zero_index_then_blist_iterator_should_return_element()
+        {
+            var count = 1;
+            var expected = new int[count].Select(_ => _random.Next()).ToArray();
+
+            var blist = new BList<int>();
+
+            foreach (var item in expected)
+                blist.Insert(0,item);
+
+            CollectionAssert.AreEqual(expected, blist);
+        }
+
+        [Test]
+        public void given_empty_blist_when_elements_are_added_individually_then_blist_count_should_equal_number_added()
+        {
+            var count = 2 << 10;
+            var expected = new int[count].Select(_ => _random.Next()).ToArray();
+
+            var blist = new BList<int>();
+
+            foreach (var item in expected)
+                blist.Add(item);
+
+            Assert.That(blist.Count, Is.EqualTo(count));
+        }
+
+        [Test]
+        public void given_empty_blist_when_elements_are_added_individually_then_blist_indexer_should_return_elements()
+        {
+            var count = 2 << 10;
+            var expected = new int[count].Select(_ => _random.Next()).ToArray();
+
+            var blist = new BList<int>();
+
+            foreach (var item in expected)
+                blist.Add(item);
+
+            for (int i = 0; i < count; i++)
+            {
+                Assert.That(expected[i], Is.EqualTo(blist[i]));
+            }
+        }
+        
+
+        [Test]
+        public void given_empty_blist_when_elements_are_added_individually_then_blist_iterator_should_return_elements()
+        {
+            var count = 1024;
+            var expected = new int[count].Select(_ => _random.Next()).ToArray();
+
+            var blist = new BList<int>();
+
+            foreach (var item in expected)
+                blist.Add(item);
+
+            CollectionAssert.AreEqual(expected, blist);
+        }
+
+        [Test]
+        public void given_empty_blist_when_elements_are_inserted_at_zero_index_individually_then_blist_iterator_should_return_elements()
+        {
+            var count = 32;
+            var initial = new int[count].Select(_ => _random.Next()).ToArray();
+            var expected = initial.Reverse().ToArray();
+
+            var blist = new BList<int>();
+
+            foreach (var item in initial)
+                blist.Insert(0, item);
+
+            Assert.That(blist.Count, Is.EqualTo(32));
+
+            CollectionAssert.AreEqual(expected, blist);
+        }
+
+        [Test]
+        public void given_existing_blist_when_elements_are_replaced_in_middle_then_blist_iterator_should_return_correct_latest_elements()
+        {
+            
+            var initial = new int[1024].Select(_ => _random.Next()).ToArray();
+            var replacement = new int[256].Select(_ => _random.Next()).ToArray();
+            var expected = initial.Take(256).Concat(replacement).Concat(initial.Skip(512)).ToArray();
+
+            var blist = new BList<int>();
+
+            foreach (var item in initial)
+                blist.Add(item);
+
+            for (int i = 0; i < 256; i++)
+                blist[256 + i] = replacement[i];
+
+            CollectionAssert.AreEqual(expected, blist);
+        }
+
+        [Test]
+        public void given_existing_blist_when_elements_are_inserted_in_middle_then_blist_iterator_should_return_correct_latest_elements()
+        {
+            var initial = new int[768].Select(_ => _random.Next()).ToArray();
+            var inserts = new int[256].Select(_ => _random.Next()).ToArray();
+            var expected = initial.Take(256).Concat(inserts).Concat(initial.Skip(256)).ToArray();
+
+            var blist = new BList<int>();
+
+            foreach (var item in initial)
+                blist.Add(item);
+
+            for (int i = 0; i < 256; i++)
+                blist.Insert(256 + i, inserts[i]);
+
+            Assert.That(blist.Count, Is.EqualTo(1024));
+
+            CollectionAssert.AreEqual(expected, blist);
+        }
+
+        [Test]
+        public void given_existing_blist_when_elements_are_inserted_at_index_zero_then_blist_iterator_should_return_correct_latest_elements()
+        {
+            var initial = new int[512].Select(_ => _random.Next()).ToArray();
+            var insertzero = new int[256].Select(_ => _random.Next()).ToArray();
+            var expected = insertzero.Reverse().Concat(initial).ToArray();
+
+            var blist = new BList<int>();
+
+            foreach (var item in initial)
+                blist.Add(item);
+
+            foreach (var item in insertzero)
+                blist.Insert(0, item);
+
+            CollectionAssert.AreEqual(expected, blist);
+        }
+
+        [Test]
+        public void given_existing_blist_when_elements_are_removed_by_index_with_zero_index_then_blist_iterator_should_return_correct_elements()
+        {
+            var initial = new int[512].Select(_ => _random.Next()).ToArray();
+            var expected = initial.Skip(32).ToArray();
+
+            var blist = new BList<int>();
+
+            foreach (var item in initial)
+                blist.Add(item);
+
+            for (int i = 0; i < 32; i++)
+            {
+                blist.RemoveAt(0);
+            }
+
+            Assert.That(blist.Count, Is.EqualTo(480));
+
+            CollectionAssert.AreEqual(expected, blist);
+        }
+
+        [Test]
+        public void given_existing_blist_when_elements_are_removed_by_element_then_blist_iterator_should_return_correct_elements()
+        {
+            var initial = new int[512].Select(_ => _random.Next()).ToArray();
+            var expected = initial.Skip(32).ToArray();
+
+            var blist = new BList<int>();
+
+            foreach (var item in initial)
+                blist.Add(item);
+
+            for (int i = 0; i < 32; i++)
+            {
+                blist.Remove(initial[i]);
+            }
+
+            Assert.That(blist.Count, Is.EqualTo(480));
+
+            CollectionAssert.AreEqual(expected, blist);
+        }
+    }
+}
